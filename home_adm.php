97e0171f3e43705c31e65f63e4bebe41443a2f1d
Carlos <carlosdanielpimentel@gmail.com>
2022-07-21 09:18:16 -0300

    Refatoramento da Home para o novo layout

--- home_adm.php
+++ home_adm.php
@@ -1,82 +1,66 @@
-<?php 
-  require_once "validador_acesso.php"
-?>
-
-
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>Tem Frete</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
-
-    <style>
-      .card-home {
-        padding: 30px 0 0 0;
-        width: 100%;
-        margin: 0 auto;
-      }
-    </style>
-  </head>
-  <body>
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="home_adm.php">
-        <img src="img/temfrete.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        Tem Frete
-      </a>
-      <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="logoff.php">SAIR</a>
-          </li>
-      </ul>
-    </nav>
-
-    <div class="container">    
-      <div class="row">
-
-        <div class="card-home">
-          <div class="card">
-            <div class="card-header">
-              Fretes
-            </div>
-            <div class="card-body">
-              <div class="row">                
-                <div class="col-6 d-flex justify-content-center">                                    
-                  <a href="criar_carga.php">                    
-                    <img src="img/criar_carga.png" width="70" height="70">                    
-                  </a>
-                </div>
-                <div class="col-6 d-flex justify-content-center">
-                  <a href="consultar_carga.php">
-                    <img src="img/consultar_carga.png" width="70" height="70">
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    </div>
-    <div class="container">    
-      <div class="row">
-
-        <div class="card-home">
-          <div class="card">
-            <div class="card-header">
-              Motorista
-            </div>
-            <div class="card-body">
-                <div class="row">                
-                <div class="col-6 d-flex justify-content-center">                  
-                  <a href="consultar_motorista.php">                    
-                    <img src="img/consultar_carga.png" width="70" height="70">
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    </div>
-  </body>
-</html>
+<?php 
+
+  require_once "validador_acesso.php"
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Área do cliente">
+    <title>Área do cliente</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen"/>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="icon" type="image/x-icon" href="img/icons/favicon.svg">
+</head>
+<body class="logado-tela-fundo">
+    <header class="cabecalho">
+        <div class="cabecalho-item-principal">
+        <h1 class="branco">TEM FRETE</h1>
+        </div>
+        <nav class="cabecalho-menu">
+            <a class="cabecalho-item" href="logoff.php"><button class="red"><img class="cabecalho-item-icone" alt="logout" src="img/icons/logout.svg">SAIR</button></a>
+        </nav>
+    </header>
+
+    <main>
+        <section class="conteudo-logado">
+            <div class="conteudo-caixa-principal">
+                <div class="conteudo-caixa-principal-texto">
+                    <h3 class="conteudo-caixa-titulo vermelho">
+                        <img class="conteudo-caixa-icone" alt="novo frete" src="img/icons/novo-grande.svg">
+                        Novo frete</h3>
+                    <h4 class="conteudo-caixa-subtitulo cinza">Para que um novo frete seja disponibilizado, basta cadastra-lo abaixo.</h4>
+                    <div class="conteudo-caixa-botao">
+                        <a href="criar_carga.php"><button class="botao-vermelho"><img class="icone" alt="consultar motoristas" src="img/icons/novo.svg">NOVO</button></a>
+                    </div>
+                </div>
+
+                <div class="conteudo-caixa-principal-texto">
+                    <h3 class="conteudo-caixa-titulo vermelho">
+                        <img class="conteudo-caixa-icone" alt="consultar fretes" src="img/icons/consultar-grande.svg">
+                        Consultar Fretes</h3>
+                    <h4 class="conteudo-caixa-subtitulo cinza">Aqui você consegue fazer uma verificação rápida de todos os fretes disponíveis.</h4>
+                    <div class="conteudo-caixa-botao">
+                        <a href="consultar_carga.php"><button class="botao-vermelho"><img class="icone" alt="consultar motoristas" src="img/icons/lupa.svg">CONSULTAR</button></a>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="conteudo-caixa-secundaria">                
+                <h3 class="conteudo-caixa-titulo vermelho">
+                    <img class="conteudo-caixa-icone" alt="consultar" src="img/icons/consultar-grande.svg">
+                    Motoristas</h3>
+                <h4 class="conteudo-caixa-subtitulo cinza">Aqui você consegue verificar todos os motoristas que temos disponíveis.</h4>
+                <div class="conteudo-caixa-botao">
+                    <a href="consultar_motorista.php"><button class="botao-vermelho"><img class="icone" alt="consultar motoristas" src="img/icons/lupa.svg">CONSULTAR</button></a>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html> 
--- validador_acesso.php
+++ validador_acesso.php
@@ -1,6 +1,12 @@
-<?php 
-  session_start();
-  if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
-    header('Location: index.php?login=erro2');
-  } 
-?>
+<?php 
+
+  session_start();
+
+  if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
+
+    header('Location: login.php?login=erro2');
+
+  } 
+
+?>
+
