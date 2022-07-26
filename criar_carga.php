eaeb1372d57744dc9eb95f67dd39b25f7d6fecdf
Carlos <carlosdanielpimentel@gmail.com>
2022-07-25 22:46:36 -0300

    Implementando criação de frete

--- criar_carga.php
+++ criar_carga.php
@@ -1,110 +1,95 @@
-<?php 
-  require_once "validador_acesso.php"
-?>
-
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>Frete de Carga</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <style>
-      .card-abrir-chamado {
-        padding: 30px 0 0 0;
-        width: 100%;
-        margin: 0 auto;
-      }
-    </style>
-  </head>
-
-  <body>
-
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="home_adm.php">
-        <img src="img/temfrete.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        Frete de Carga
-      </a>
-      <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="logoff.php">SAIR</a>
-          </li>
-      </ul> 
-    </nav>
-
-    <?php if(isset($_GET['inclusao']) && $_GET['inclusao'] == 1 ) { ?>
-      <div class="bg-success pt-2 text-white d-flex justify-content-center">
-        <h5>Carga inserida com sucesso!</h5>
-      </div>
-    <?php } ?>
-
-    <div class="container">    
-      <div class="row">
-
-        <div class="card-abrir-chamado">
-          <div class="card">
-            <div class="card-header">
-              Criação da Carga
-            </div>
-            <div class="card-body">
-              <div class="row">
-                <div class="col">
-                  
-                  <form method="POST" action="frete_controller.php?acao=inserir">
-                    <div class="form-group">
-                      <label>Título Frete</label>
-                      <input name="titulo" type="text" class="form-control" placeholder="Título">
-                    </div>
-                    
-                    <div class="form-group">
-                      <label>Tipo caminhão</label>
-                      <select name="tp_caminhao" class="form-control">
-                        <option value="3/4">3/4</option>
-                        <option value="TOCO">TOCO</option>
-                        <option value="TRUCK">TRUCK</option>
-                        <option value="BITRUCK">BITRUCK</option>
-                        <option value="CARRETA">CARRETA</option>
-                        <option value="BITREM">BITREM</option>
-                        <option value="RODOTREM">RODOTREM</option>
-                      </select>
-                    </div>
-                    
-                    <div class="form-group">
-                      <label>Peso da Carga:</label>
-                      <input name="peso_carga" type="text" class="form-control" placeholder="Peso da Carga">
-                    </div>
-
-                    <div class="boxtut">
-                      <label>Local de Coleta</label>
-                      <textarea name="local_coleta" placeholder="Endereço, Número, Bairro, Cidade, Estado" class="form-control" rows="2"></textarea>
-                    </div>
-
-                    <div class="form-group">
-                      <label>Local de Destino</label>
-                      <textarea name="local_entrega" placeholder="Endereço, Número, Bairro, Cidade, Estado" class="form-control" rows="2"></textarea>
-                    </div>
-
-                    <div class="form-group">
-                      <label>Valor do Frete:</label>
-                      <input name="valor" type="text" class="form-control" placeholder="R$ Frete">
-                    </div>
-
-                    <div class="row mt-5">
-                      <div class="col-6">
-                        <a href="home_adm.php" class="btn btn-lg btn-warning btn-block">Voltar</a>
-                      </div>
-
-                      <div class="col-6">
-                        <button class="btn btn-lg btn-info btn-block" type="submit">Salvar</button>
-                      </div>
-                    </div>
-                  </form>
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    </div>
-  </body>
+<?php 
+
+  require_once "validador_acesso.php"
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Cadastrar um novo frete">
+    <title>Área do Cliente - Novo Frete</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen"/>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="icon" type="image/x-icon" href="img/icons/favicon.svg">
+    
+</head>
+<body class="logado-tela-fundo">
+    <header class="cabecalho">
+        <div class="cabecalho-item-principal">
+        <h1 class="branco">TEM FRETE</h1>
+        </div>
+        <nav class="cabecalho-menu">
+            <a class="cabecalho-item" href=""><button class="red"><img class="cabecalho-item-icone" alt="logout" src="img/icons/logout.svg">SAIR</button></a>
+        </nav>
+    </header>
+
+    <div class="novo-frete">
+      <?php if(isset($_GET['inclusao']) && $_GET['inclusao'] == 1 ) { ?>
+        <div class="bg-success pt-2 text-white d-flex justify-content-center">
+          <h5>Carga inserida com sucesso!</h5>
+        </div>
+      <?php } ?>
+    
+      <section class="novo-frete-conteudo">
+        <h3 class="login-conteudo-titulo vermelho"><strong>Digite todas as informações para cadastrar um novo frete!</strong></h3>
+        <form class="login-formulario" method="POST" action="frete_controller.php?acao=inserir">
+
+            <div class="login-formulario-barra">
+                <img class="icone" alt="Título do frete" src="img/icons/titulo-frete.svg">
+                <input class="login-conteudo-input cinza" type="text" placeholder="Título do frete" name="titulo-frete" required>
+            </div>
+            
+            <div class="login-formulario-barra">
+                <img class="icone" alt="Tipo do caminhao" src="img/icons/tipo-caminhao.svg">
+                <!-- <input class="login-conteudo-input cinza" type="text" placeholder="Tipo de caminhão" name="tipo-caminhao" required> -->
+                <select name="tp_caminhao" class="login-conteudo-input cinza" placeholder="Tipo">
+                  <option value="3/4">3/4</option>
+                  <option value="TOCO">TOCO</option>
+                  <option value="TRUCK">TRUCK</option>
+                  <option value="BITRUCK">BITRUCK</option>
+                  <option value="CARRETA">CARRETA</option>
+                  <option value="BITREM">BITREM</option>
+                  <option value="RODOTREM">RODOTREM</option>
+                </select>
+            </div>
+
+            <div class="login-formulario-barra">
+                <img class="icone" alt="Peso da carga" src="img/icons/peso-carga.svg">
+                <input class="login-conteudo-input cinza" type="text" placeholder="Peso da carga" name="peso-carga" required>
+            </div>
+
+            <div class="login-formulario-barra">
+                <img class="icone" alt="Local coleta" src="img/icons/local-coleta.svg">
+                <input class="login-conteudo-input cinza" type="text" placeholder="Local de coleta, ex: endereço, número, bairro, cidade e estado" name="local-coleta" required>
+            </div>
+
+            <div class="login-formulario-barra">
+                <img class="icone" alt="Local destino" src="img/icons/local-destino.svg">
+                <input class="login-conteudo-input cinza" type="text" placeholder="Local do destino, ex: endereço, número, bairro, cidade e estado" name="local-destino" required>
+            </div>
+
+            <div class="login-formulario-barra">
+                <img class="icone" alt="Valor do frete" src="img/icons/valor-frete.svg">
+                <input class="login-conteudo-input cinza" type="text" placeholder="Valor do frete R$" name="valor-frete" required>
+            </div>
+
+            <div class="login-formulario-barra">
+                <a href="logado.html">
+                <button class="botao-vermelho branco" type="button">
+                    <img class="icone" alt="voltar" src="img/icons/voltar.svg">
+                    VOLTAR</button>
+                </a>
+
+                <button class="botao-vermelho branco" type="submit">
+                    <img class="icone" alt="salvar" src="img/icons/salvar.svg">
+                    SALVAR</button>
+            </div>
+        </form>
+    </section>
+</div>
+
+</body>
 </html>
