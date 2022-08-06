c421eade4b0b697afd148a96fa05df7453f3e8c8
Carlos <carlosdanielpimentel@gmail.com>
2022-08-06 14:25:55 -0300

    Ajuste do layout da consulta de frete

--- consultar_carga.php
+++ consultar_carga.php
@@ -1,120 +1,214 @@
-<?php require_once "validador_acesso.php" ?>
-
-<?php
-  $acao = 'recuperar';
-  require 'frete_controller.php';
-  
-?>
-
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>Consulta de Cargas</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/estilo.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-
-    <style>
-      .card-consultar-chamado {
-        padding: 30px 0 0 0;
-        width: 100%;
-        margin: 0 auto;
-      }
-    </style>
-  </head>
-
-  <script>
-    function remover(id) {
-        location.href = 'consultar_carga.php?acao=remover&id='+id;
-      }
-
-    function inativar(id) {
-        location.href = 'consultar_carga.php?acao=inativar&id='+id;
-      }
-
-    function ativar(id) {
-        location.href = 'consultar_carga.php?acao=ativar&id='+id;
-      } 
-
-  </script>
-
-  <body>
-
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="home_adm.php">
-        <img src="img/temfrete.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        Consulta de Cargas
-      </a>
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="logoff.php">SAIR</a>
-        </li>
-      </ul>
-    </nav>
-
-    <?php if( isset($_GET['msg']) && $_GET['msg'] == 'sucesso' ) { ?>
-      <div class="bg-success pt-2 text-white d-flex justify-content-center">
-        <h5>Processo executado com Sucesso!!!</h5>
-      </div>
-    <?php } ?>
-
-    <?php if( isset($_GET['msg']) && $_GET['msg'] == 'erro' ) { ?>
-      <div class="bg-danger pt-2 text-white d-flex justify-content-center">
-        <h5>Problemas ao executar processo!!!</h5>
-      </div>
-    <?php } ?>
-    
-
-    <div class="container">    
-      <div class="row">
-
-        <div class="card-consultar-chamado">
-          <div class="card">
-            <div class="card-header">
-              Cargas Cadastradas
-            </div>
-            
-            <div class="card-body">
-
-              <?php foreach($frete as $user => $frete) { ?>
-
-                <div class="card mb-3 bg-light">                  
-                  <div class="card-body">
-                    <?php if($frete->status == 1) { ?>
-                        <i style="margin-left: 90%;" class="fas fa-check-square fa-lg text-success" onclick="inativar(<?= $frete->id_frete ?>)"> Ativo</i>                    
-                    <?php } ?>
-                    <?php if($frete->status == 2) { ?>
-                        <i style="margin-left: 90%;" class="fas fa-check-square fa-lg text-danger" onclick="ativar(<?= $frete->id_frete ?>)"> Inátivo</i>                    
-                    <?php } ?>  
-                    <h5 class="card-title" id="frete_<?= $frete->id_frete ?>">Titulo: <?= $frete->titulo?></h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Caminhão: <?= $frete->tp_caminhao ?></h6>
-                    <h6 class="card-subtitle mb-2 text-muted">Peso: <?= $frete->peso_carga ?></h6>
-                    <h6 class="card-subtitle mb-2 text-muted">Local de Coleta: <?= $frete->local_coleta ?></h6>
-                    <h6 class="card-subtitle mb-2 text-muted">Local de Entrega: <?= $frete->local_entrega ?></h6>
-                    <p class="card-text">Valor: <?= $frete->valor ?></p>                    
-                  </div>
-                  <div class="row mt-5">
-                      <div class="col-6">
-                        <a href="#" class="btn btn-lg btn-danger btn-block" onclick="remover(<?= $frete->id_frete ?>)"></i>Excluir</a>
-                      </div>
-                      <div class="col-6">
-                        <a href="editar_carga.php?id=<?= $frete->id_frete ?>" class="btn btn-lg btn-info btn-block">Editar</a>
-                      </div>                        
-                    </div>
-                </div>
-              <?php } ?>
-
-              <div class="row mt-5">
-                <div class="col-6">
-                  <a class="btn btn-lg btn-warning btn-block" href="home_adm.php">Voltar</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </body>
+<?php require_once "validador_acesso.php" ?>
+<?php
+  $acao = 'recuperar';
+  require 'frete_controller.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Consultar todos os fretes cadastradis">
+    <title>Área do Cliente - Consultar Frete</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen"/>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="icon" type="image/x-icon" href="img/icons/favicon.svg">
+</head>
+<script>
+    function remover(id) {
+        location.href = 'consultar_carga.php?acao=remover&id='+id;
+      }
+    function inativar(id) {
+        location.href = 'consultar_carga.php?acao=inativar&id='+id;
+      }
+    function ativar(id) {
+        location.href = 'consultar_carga.php?acao=ativar&id='+id;
+      } 
+</script>
+
+<body class="logado-tela-fundo">
+    <header class="cabecalho">
+        <div class="cabecalho-item-principal">
+        <h1 class="branco">TEM FRETE</h1>
+        </div>
+        <nav class="cabecalho-menu">
+          <a class="cabecalho-item"  href="logoff.php"><button class="red"><img class="cabecalho-item-icone" alt="logout" src="img/icons/logout.svg">SAIR</button></a>
+        </nav>
+    </header>
+
+    <?php if( isset($_GET['msg']) && $_GET['msg'] == 'sucesso' ) { ?>
+      <div class="bg-success pt-2 text-white d-flex justify-content-center">
+        <h5>Processo executado com Sucesso!!!</h5>
+      </div>
+    <?php } ?>
+    <?php if( isset($_GET['msg']) && $_GET['msg'] == 'erro' ) { ?>
+      <div class="bg-danger pt-2 text-white d-flex justify-content-center">
+        <h5>Problemas ao executar processo!!!</h5>
+      </div>
+    <?php } ?>
+
+    <?php $contador = 0; ?>
+
+    <?php foreach($frete as $user => $frete) { ?>
+      
+      <!-- verificar se contador é igual a zero -->
+      <?php if($contador == 0) { ?>
+        <section class="conteudo">
+      <?php } ?>
+
+      <?php if($frete->status == 1) { ?>
+        <div class="conteudo-caixa-consultar-frete">
+
+            <div class="conteudo-caixa-consultar-status-frete" onclick="ativar(<?= $frete->id_frete ?>)">
+                <img class="icone" src="img/icons/status-ativo.svg">
+                <p class="preto">ATIVO</p>
+            </div>
+
+            <div class="conteudo-caixa-consultar-frete-linha">
+                <div class="conteudo-caixa-consultar-frete-imagem">
+                    <img class="icone" alt="Titulo do frete" src="img/icons/titulo-frete.svg">
+                    <p class="conteudo-caixa-consultar-frete-texto cinza">Título do frete</p>
+                </div>
+                <p class="conteudo-caixa-titulo preto"><strong><?= $frete->titulo?></strong></p>
+            </div>
+
+            <div class="conteudo-caixa-consultar-frete-linha">
+                <div class="conteudo-caixa-consultar-frete-imagem">
+                    <img class="icone" alt="Tipo do caminhao" src="img/icons/tipo-caminhao.svg">
+                    <p class="conteudo-caixa-consultar-frete-texto cinza">Tipo do caminhão</p>
+                </div>
+                <p class="conteudo-caixa-titulo preto"><?= $frete->tp_caminhao ?></p>
+            </div>
+
+            <div class="conteudo-caixa-consultar-frete-linha">
+                <div class="conteudo-caixa-consultar-frete-imagem">
+                    <img class="icone" alt="Peso da carga" src="img/icons/peso-carga.svg">
+                    <p class="conteudo-caixa-consultar-frete-texto cinza">Peso da carga</p>
+                </div>
+                <p class="conteudo-caixa-titulo preto"><?= $frete->peso_carga ?> KG</p>
+            </div>
+
+            <div class="conteudo-caixa-consultar-frete-linha">
+                <div class="conteudo-caixa-consultar-frete-imagem">
+                    <img class="icone" alt="Local da coleta" src="img/icons/local-coleta.svg">
+                    <p class="conteudo-caixa-consultar-frete-texto cinza">Local de coleta</p>
+                </div>
+                <p class="conteudo-caixa-titulo preto"><?= $frete->local_coleta ?></p>
+            </div>
+
+            <div class="conteudo-caixa-consultar-frete-linha">
+                <div class="conteudo-caixa-consultar-frete-imagem">
+                    <img class="icone" alt="Local da coleta" src="img/icons/local-destino.svg">
+                    <p class="conteudo-caixa-consultar-frete-texto cinza">Local de destino</p>
+                </div>
+                <p class="conteudo-caixa-titulo preto"><?= $frete->local_entrega ?></p>
+            </div>
+
+            <div class="conteudo-caixa-consultar-frete-linha">
+                <div class="conteudo-caixa-consultar-frete-imagem">
+                    <img class="icone" alt="Valor do frete" src="img/icons/valor-frete.svg">
+                    <p class="conteudo-caixa-consultar-frete-texto cinza">Valor R$</p>
+                </div>
+                <p class="conteudo-caixa-titulo preto"><?= $frete->valor ?> R$</p>
+            </div>
+
+            <div class="login-formulario-barra">
+              <a href="#" onclick="remover(<?= $frete->id_frete ?>)">
+                <button class="botao-preto branco" type="button">
+                  <img class="icone" alt="excluir" src="img/icons/excluir.svg">
+                  EXCLUIR
+                </button>
+              </a>
+              <a href="editar_carga.php?id=<?= $frete->id_frete ?>">
+               <button class="botao-preto branco" type="button">
+                  <img class="icone" alt="editar" src="img/icons/editar.svg">
+                  EDITAR
+                </button>
+              </a>
+            </div>
+        </div>
+      <?php } ?>
+
+      <?php if($frete->status == 2) { ?>
+        <div class="conteudo-caixa-consultar-frete"  onclick="inativar(<?= $frete->id_frete ?>)">
+            <div class="conteudo-caixa-consultar-status-frete">
+                <img class="icone" src="img/icons/status-inativo.svg">              
+                  <p class="laranja">INATIVO</p>
+            </div>
+
+            <div class="conteudo-caixa-consultar-frete-linha">
+                <div class="conteudo-caixa-consultar-frete-imagem">
+                    <img class="icone" alt="Titulo do frete" src="img/icons/titulo-frete.svg">
+                    <p class="conteudo-caixa-consultar-frete-texto cinza">Título do frete</p>
+                </div>
+                <p class="conteudo-caixa-titulo laranja"><strong><?= $frete->titulo?></strong></p>
+            </div>
+
+            <div class="conteudo-caixa-consultar-frete-linha">
+                <div class="conteudo-caixa-consultar-frete-imagem">
+                    <img class="icone" alt="Tipo do caminhao" src="img/icons/tipo-caminhao.svg">
+                    <p class="conteudo-caixa-consultar-frete-texto cinza">Tipo do caminhão</p>
+                </div>
+                <p class="conteudo-caixa-titulo laranja"><?= $frete->tp_caminhao ?></p>
+            </div>
+
+            <div class="conteudo-caixa-consultar-frete-linha">
+                <div class="conteudo-caixa-consultar-frete-imagem">
+                    <img class="icone" alt="Peso da carga" src="img/icons/peso-carga.svg">
+                    <p class="conteudo-caixa-consultar-frete-texto cinza">Peso da carga</p>
+                </div>
+                <p class="conteudo-caixa-titulo laranja"><?= $frete->peso_carga ?> KG</p>
+            </div>
+
+            <div class="conteudo-caixa-consultar-frete-linha">
+                <div class="conteudo-caixa-consultar-frete-imagem">
+                    <img class="icone" alt="Local da coleta" src="img/icons/local-coleta.svg">
+                    <p class="conteudo-caixa-consultar-frete-texto cinza">Local de coleta</p>
+                </div>
+                <p class="conteudo-caixa-titulo laranja"><?= $frete->local_coleta ?></p>
+            </div>
+
+            <div class="conteudo-caixa-consultar-frete-linha">
+                <div class="conteudo-caixa-consultar-frete-imagem">
+                    <img class="icone" alt="Local da coleta" src="img/icons/local-destino.svg">
+                    <p class="conteudo-caixa-consultar-frete-texto cinza">Local de destino</p>
+                </div>
+                <p class="conteudo-caixa-titulo laranja"><?= $frete->local_entrega ?></p>
+            </div>
+
+            <div class="conteudo-caixa-consultar-frete-linha">
+                <div class="conteudo-caixa-consultar-frete-imagem">
+                    <img class="icone" alt="Valor do frete" src="img/icons/valor-frete.svg">
+                    <p class="conteudo-caixa-consultar-frete-texto cinza">Valor R$</p>
+                </div>
+                <p class="conteudo-caixa-titulo laranja"><?= $frete->valor ?> R$</p>
+            </div>
+
+            <div class="login-formulario-barra">
+              <a href="#" onclick="remover(<?= $frete->id_frete ?>)">
+                <button class="botao-laranja branco" type="button">
+                    <img class="icone" alt="excluir" src="img/icons/excluir.svg">
+                    EXCLUIR</button>
+              </a>
+              <a href="editar_carga.php?id=<?= $frete->id_frete ?>">
+                <button class="botao-laranja branco" type="button">
+                    <img class="icone" alt="editar" src="img/icons/editar.svg">
+                    EDITAR</button>
+              </a>
+            </div>
+        </div>
+      <?php } ?>
+
+      <?php if($contador == 1) { ?>
+        </section>        
+      <?php } ?>
+      
+      <?php $contador ++; ?>            
+
+      <?php if($contador == 2) { ?>        
+        <?php $contador =0; ?>            
+      <?php } ?>
+    <?php } ?>
+</body>
 </html>
