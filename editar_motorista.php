a12d91aa9c1e7c0022fc50e8a246db38ff87c1b0
Carlos <carlosdanielpimentel@gmail.com>
2022-08-07 09:09:45 -0300

    Ajuste no layout de edição de motorista

--- editar_motorista.php
+++ editar_motorista.php
@@ -1,110 +1,91 @@
-<?php require_once "validador_acesso.php" ?>
-
-<?php
-  $acao = 'carregar';  
-  require 'motorista_controller.php';  
-?>
-
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>Editar Motorista</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="js/jquery.mask.min.js"></script>
-
-    <style>
-      .card-abrir-chamado {
-        padding: 30px 0 0 0;
-        width: 100%;
-        margin: 0 auto;
-      }
-    </style>
-  </head>
-
-  <script type="text/javascript">
-    $(document).ready(function(){
-      $("#cpf").mask("000.000.000-00");
-      $("#telefone").mask("(00) 00000-0000");
-      $("#placa").mask("AAA-0A00");
-
-    })
-  </script>
-
-  <body>
-
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="home_adm.php">
-        <img src="img/temfrete.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        Motorista
-      </a>
-      <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="logoff.php">SAIR</a>
-          </li>
-      </ul> 
-    </nav>  
-
-    <div class="container">    
-      <div class="row">
-
-        <div class="card-abrir-chamado">
-          <div class="card">
-            <div class="card-header">
-              Edição do Motorista
-            </div>
-            <div class="card-body">
-              <div class="row">
-                <div class="col">
-                  
-                  <form method="POST" action="motorista_controller.php?acao=atualizar">
-
-                    <?php foreach($motorista as $user => $motorista) { ?>
-
-
-                    <input name="id" type="hidden" class="form-control" placeholder="id" value="<?= $motorista->id?>">
-
-                    <div class="form-group">
-                      <label>Nome Motorista</label>
-                      <input name="nome" type="text" class="form-control" placeholder="Nome Motorista" value="<?= $motorista->nome?>">
-                    </div>
-                    
-                    
-                    <div class="form-group">
-                      <label>Telefone</label>
-                      <input name="telefone" id="telefone"0 type="text" class="form-control" placeholder="Telefone" value="<?= $motorista->telefone?>">
-                    </div>
-
-                    <div class="form-group">
-                      <label>CPF</label>
-                      <input name="cpf" id="cpf" placeholder="CPF" class="form-control" value="<?=$motorista->cpf?>"></input>
-                    </div>
-
-                    <div class="form-group">
-                      <label>Placa</label>
-                      <input name="placa" id="placa" placeholder="Placa do Veículo" class="form-control" value="<?=$motorista->placa?>"></input>
-                    </div>
-
-                    <div class="row mt-5">
-                      <div class="col-6">
-                        <a href="home_adm.php" class="btn btn-lg btn-warning btn-block">Voltar</a>
-                      </div>
-
-                      <div class="col-6">
-                        <button class="btn btn-lg btn-info btn-block" type="submit">Salvar</button>
-                      </div>
-                    </div>
-
-                     <?php } ?>
-                  </form>
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    </div>
-  </body>
+<?php require_once "validador_acesso.php" ?>
+<?php
+  $acao = 'carregar';  
+  require 'motorista_controller.php';  
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Cadastrar um novo frete">
+    <title>Área do Cliente - Alterar cadastro motorista</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen"/>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="icon" type="image/x-icon" href="img/icons/favicon.svg">
+</head>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+      $("#cpf").mask("000.000.000-00");
+      $("#telefone").mask("(00) 00000-0000");
+      $("#placa").mask("AAA-0A00");
+    })
+</script>
+
+<body class="logado-tela-fundo">
+<header class="cabecalho">
+        <div class="cabecalho-item-principal">
+        <a href="home_adm.php"><h1 class="branco">TEM FRETE</h1></a>
+        </div>
+        <nav class="cabecalho-menu">
+          <a class="cabecalho-item"  href="logoff.php"><button class="red"><img class="cabecalho-item-icone" alt="logout" src="img/icons/logout.svg">SAIR</button></a>
+        </nav>
+    </header>
+
+    <div class="novo-frete">
+    
+    <section class="alterar-motoristas">
+        <h3 class="login-conteudo-titulo vermelho"><strong>Altere o cadastro do motorista!</strong></h3>
+        <form class="login-formulario" method="POST" action="motorista_controller.php?acao=atualizar">
+
+          <?php foreach($motorista as $user => $motorista) { ?>
+
+            <input name="id" type="hidden" class="form-control" placeholder="id" value="<?= $motorista->id?>">
+
+            <div class="login-formulario-barra">
+                <img class="icone" alt="Nome do motorista" src="img/icons/nome-motorista.svg">
+                <input class="login-conteudo-input cinza" type="text" placeholder="Nome" name="nome" value="<?= $motorista->nome?>" required>
+            </div>
+            
+            <div class="login-formulario-barra">
+                <img class="icone" alt="Telefone" src="img/icons/telefone-motorista.svg">
+                <input class="login-conteudo-input cinza" type="text" placeholder="Telefone" name="telefone" id="telefone" value="<?= $motorista->telefone?>" required>
+            </div>
+
+            <div class="login-formulario-barra">
+                <img class="icone" alt="CPF" src="img/icons/cpf-motorista.svg">
+                <input class="login-conteudo-input cinza" type="text" placeholder="CPF" name="cpf" id="cpf" value="<?=$motorista->cpf?>" required>
+            </div>
+
+            <div class="login-formulario-barra">
+                <img class="icone" alt="Placa" src="img/icons/tipo-caminhao.svg">
+                <input class="login-conteudo-input cinza" type="text" placeholder="Placa" name="placa" id="placa" value="<?=$motorista->placa?>" required>
+            </div>
+
+            <!-- <div class="login-formulario-barra">
+                <img class="icone" alt="Tipo motorista" src="img/icons/tipo-motorista.svg">
+                <input class="login-conteudo-input cinza" type="text" placeholder="Tipo motorista" name="tipo-motorista" required>
+            </div> -->
+
+            <div class="login-formulario-barra">
+              <a href="home_adm.php">
+                <button class="botao-vermelho branco" type="button">
+                    <img class="icone" alt="voltar" src="img/icons/voltar.svg">
+                    VOLTAR</button>
+              </a>
+
+                <button class="botao-vermelho branco" type="submit">
+                    <img class="icone" alt="salvar" src="img/icons/salvar.svg">
+                    SALVAR</button>
+            </div>
+
+            <?php } ?>
+        </form>
+    </section>
+</div>
+
+</body>
 </html>
